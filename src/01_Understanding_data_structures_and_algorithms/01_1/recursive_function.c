cac69e2211eb78349fa5f46d0703d603ad3f18a5
jiwonsudo <wondev112@gmail.com>
2024-11-16 04:02:42 +0000

    Style: change function case in recursive, 01-1

--- src/01_Understanding_data_structures_and_algorithms/01_1/recursive_function.c
+++ src/01_Understanding_data_structures_and_algorithms/01_1/recursive_function.c
@@ -1,17 +1,17 @@
 // 재귀함수 연습
 #include <stdio.h>
 
-int sumFrom0(int number);
+int SumFromZero(int number);
 
 void main() {
 	int numberToSum = 10;
-	printf("0에서 %d까지의 합: %d\n", numberToSum, sumFrom0(numberToSum));
+	printf("0에서 %d까지의 합: %d\n", numberToSum, SumFromZero(numberToSum));
 }
 
 // 재귀함수
-int sumFrom0(int number) {
+int SumFromZero(int number) {
 	if (number > 0) {  // 재귀함수에는 탈출 조건이 있어야 함
-		return number + sumFrom0(number - 1);  // 함수를 재호출
+		return number + SumFromZero(number - 1);  // 함수를 재호출
 	} else {
 		return number;  // 탈출 시
 	}
