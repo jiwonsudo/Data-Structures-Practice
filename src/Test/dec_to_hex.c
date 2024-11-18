33ce04281d7488b82024f6a0e564d6a4929e36f2
jiwonsudo <wondev112@gmail.com>
2024-11-18 01:54:11 +0000

    Refactor: divide decTohex as function

--- src/Test/dec_to_hex.c
+++ src/Test/dec_to_hex.c
@@ -1,49 +1,26 @@
 #include <stdio.h>
 
-void main() {
-	int dec = 72010265;
+void decToHex(int dec, int arr[]) {
 	int rem = 16;
-	int arr[100] = {0,}; 
-	
-	for(int i = 0; dec > 0; i++) {
+	int i = 0;
+	while (dec > 0) {
 		rem = dec % 16;
 		dec = dec / 16;  // int끼리의 나눗셈이므로 자동으로 몫으로 변환
-		arr[i] = rem;
 		
+		arr[i] = (rem < 10) ? '0' + rem : 'A' + (rem - 10);
+		i++;
 	}
+}
+
+void main() {
+	int arr[100] = {0,}; 
+	int dec = 72010265;
+	
+	decToHex(dec, arr);
 	
 	printf("DECIMAL: %d, HEXADECIMAL: ", dec);
-	int isEmptyArr = 1;
-	
 	for (int i = 99; i >= 0; i--) {
-		if (arr[i] != 0) {
-			isEmptyArr = 0;
-		}
-		if (isEmptyArr == 0) {
-			switch (arr[i]) {
-			case 10:
-				printf("A");
-				break;
-			case 11:
-				printf("B");
-				break;
-			case 12:
-				printf("C");
-				break;
-			case 13:
-				printf("D");
-				break;
-			case 14:
-				printf("E");
-				break;
-			case 15:
-				printf("F");
-				break;
-			default:
-				printf("%d", arr[i]);
-				break;
-			}
-		}
+		printf("%c", arr[i]);
 	}
 	
 	printf("\n");
