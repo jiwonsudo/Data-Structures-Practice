e9a745478b4d1192fbfd7bc5c5dd1cdc6fdee782
jiwonsudo <wondev112@gmail.com>
2024-11-29 07:12:18 +0000

    Create: chap04 DLinkedListSortMain

---
+++ src/04-Linked-List-2/DLinkedListSort/DLinkedListSortMain.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "DLinkedList.h"
+
+int isFirstArgFirst(int d1, int d2){
+	if (d1 < d2)
+		return 0;  // d1 goes first
+	else
+		return 1;  // d2 goes first
+}
+
+int main() {
+	List list;
+	int data;
+	ListInit(&list);
+	
+	SetSortRule(&list, isFirstArgFirst);  // 정렬기준등록
+	
+	LInsert(&list, 11); LInsert(&list, 22);
+	LInsert(&list, 33); LInsert(&list, 22);
+	LInsert(&list, 11);
+	
+	printf("현재 데이터 수: %d\n", LCount(&list));
+	printf("현재 데이터: "); PrintAllItems(&list);
+	
+	if (LFirst(&list, &data)) {
+		if (data == 33)
+			LRemove(&list);
+		while (LNext(&list, &data)) {
+			if (data == 33)
+				LRemove(&list);
+		}
+	}	
+	
+	printf("현재 데이터 수: %d\n", LCount(&list));
+	printf("현재 데이터: "); PrintAllItems(&list);
+	
+	return 0;
+}
